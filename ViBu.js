597a235f71263d927980628891b797f8681b4f2a
unknown <vidarberge95@gmail.com>
2020-09-18 18:57:49 +0200

    Update the tab system

--- ViBu.js
+++ ViBu.js
@@ -1,3 +1,4 @@
+
 //Get the searchbar
 let searchBar = document.getElementsByClassName("search");
 //Execute function when user scrolls the page
@@ -15,26 +16,27 @@ function checkSticky (){
 };
     
 
-function selectComment (evt, commentId) {
+function selectComment (evt, tabNum, postNum, user) {
     //Declare variables
-    let tabContent, tabLink, id;
+    let postContent, tabLink, clicked, classCombine;
 
-    //Get all elements with class tabcontent and hide them
-    tabContent = document.getElementsByClassName("tabContent");
-    for (let i = 0; i < tabContent.length; i++){
-        tabContent[i].classList.add("hidden");
+
+    //Get all elements with class postNum and hide them 
+    postContent = document.getElementsByClassName(postNum);
+    for (let i = 0; i < postContent.length; i++){
+        postContent[i].classList.add("hidden");
     };
 
-    //Get all elements with class tabLink and deactivate them
-    tabLink = document.getElementsByClassName("tabLink");
+    //Get all elements with class tabNum and deactivate them
+    tabLink = document.getElementsByClassName(tabNum);
     for (let i = 0; i < tabLink.length; i++) {
         tabLink[i].classList.remove("active");
     };
 
-    //Show current tab and activate the button
-    id = document.getElementById(commentId);
-    console.log(commentId);
-    id.classList.remove("hidden");
+    //Show clicked tab and activate the link
+    classCombine = `${postNum} ${user}`;
+    clicked = document.getElementsByClassName(classCombine);
+    clicked[0].classList.remove("hidden");
     evt.currentTarget.className += " active";
 }
 
