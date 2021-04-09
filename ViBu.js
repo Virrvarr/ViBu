ccfd03e69eee14c9cf835d083b81b1e50483f643
viktor hesselbom <viktor@hesselbom.net>
2021-04-09 19:05:11 +0200

    Update ViBu.js

--- ViBu.js
+++ ViBu.js
@@ -1,77 +1,58 @@
 const searchBar = document.getElementById("searchBar");
-const posts = document.getElementsByClassName("post");
-const postSearchString = [];
 
-console.log(posts);
-
-//Create an array of objects that links post number with the searchable textcontent in the post.
-//My thought behind this is that I'll only have to do this once per load, instead of everytime the search changes.
-for (let i = 0; i < posts.length; i++){
-    //Create the string
-    let postString = "";
-
-    //Chack if it's the info post and leave the string empty in that case
-    if (posts[i].id==="info"){
-        postSearchString.push({nr: i, string: postString.toLowerCase()});
-
-    } else if (posts[i].classList.contains("completed")) {    
-        postString += "completed";
-        //Add the post title
-        postString += posts[i].children[0].textContent;
-        //Push the object into postSearchString
-        postSearchString.push({nr: i, string: postString.toLowerCase()});
-    } else {   
-        //Add the post title
-        postString += posts[i].children[0].textContent;
-        //Add the text content of the tabs excluding "Description"
-        for (let j = 1; j < posts[i].children[2].children.length; j++){
-            postString += posts[i].children[2].children[j].textContent;
-        }
-        //Push the object into postSearchString
-        postSearchString.push({nr: i, string: postString.toLowerCase()});
-    }
-};
-
-console.log(postSearchString);
-
-searchBar.addEventListener('keyup', (evt)=> {
-    //Show all posts
-    for (let post of posts){
-        post.classList.remove("hidden")
-    }
-    //Get the search string
-    let searchString = evt.target.value.toLowerCase();
-
-    //Hide all posts that don't contain the search 
-    for (let pair of postSearchString){
-        if (!pair.string.includes(searchString)){
-            posts[pair.nr].classList.add("hidden");
-        }
-    }
-})
-
-function selectComment (evt, tabNum, postNum, user) {
-    //Declare variables
-    let postContent, tabLink, clicked, classCombine;
-
-
-    //Get all elements with class postNum and hide them 
-    postContent = document.getElementsByClassName(postNum);
-    for (let i = 0; i < postContent.length; i++){
-        postContent[i].classList.add("hidden");
-    };
-
-    //Get all elements with class tabNum and deactivate them
-    tabLink = document.getElementsByClassName(tabNum);
-    for (let i = 0; i < tabLink.length; i++) {
-        tabLink[i].classList.remove("active");
-    };
-
-    //Show clicked tab and activate the link
-    classCombine = `${postNum} ${user}`;
-    clicked = document.getElementsByClassName(classCombine);
-    clicked[0].classList.remove("hidden");
-    evt.currentTarget.className += " active";
+function getPostContentPart (post, part) {
+    //const elements = post.querySelectorAll(`[data-${part}]`)
+    const elements = post.querySelectorAll('[data-' + part + ']')
+    return Array.from(elements)
+        .map(element => element.textContent)
+        .join('')
 }
 
+const posts = Array.from(document.getElementsByClassName("post"))
+  .map(post => ({
+    element: post,
+    string: (
+      (post.classList.contains("completed") ? "completed" : "") +
+      getPostContentPart(post, 'title') +
+      getPostContentPart(post, 'tab')
+    ).toLowerCase()
+  }))
 
+// Setup tabs
+for (let post of posts) {
+    const tabs = post.element.querySelectorAll('.tab > button')
+    const tabContents = post.element.querySelectorAll('.tabContent')
+    
+    for (let tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
+        tabs[tabIndex].addEventListener('click', () => {
+            for (let tabContentIndex = 0; tabContentIndex < tabContents.length; tabContentIndex++) {
+                /*if (tabContentIndex !== tabIndex) {
+                    tabContents[tabIndex].classList.add('hidden')
+                } else [
+                    tabContents[tabIndex].classList.remove('hidden')
+                }*/
+                tabs[tabIndex].classList.toggleClass('active', tabContentIndex === tabIndex)
+                tabContents[tabIndex].classList.toggleClass('hidden', tabContentIndex !== tabIndex)
+            }
+        })
+    }
+}
+
+function showAllPosts () {
+    for (let post of posts) {
+        post.element.classList.remove("hidden")
+    }
+}
+
+function hidePostsNotContainingSearchQuery (query) {
+    for (let post of posts){
+        if (!post.string.includes(query)){
+            post.element.classList.add("hidden");
+        }
+    }
+}
+
+searchBar.addEventListener('keyup', (evt)=> {
+    showAllPosts()
+    hidePostsNotContainingSearchQuery(evt.target.value.toLowerCase())
+})
